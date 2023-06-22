e7cd7a0e1b01d2eaf08abad0be8eaf95135065ae
Micahel <michael.kosinski@renesas.como>
2023-06-22 10:36:42 -0700

    updated script to latest version and added ARMNN support

--- rzg2l_build.sh
+++ rzg2l_build.sh
@@ -9,8 +9,10 @@ popd
 
 echo "##############################################################################################"
 
+ENABLE_AI_FRAMEWORK=1
 
-WORK_DIR=rzg2l
+WORK_DIR=${PWD}/$1
+
 echo "Work Directory : ${WORK_DIR}"
 echo " Packages :"
 
@@ -39,21 +41,55 @@ tar -zxvf ${Graphic::-4}/meta-rz-features_graphics_*.tar.gz
 unzip ${SRC_DIR}/${VideoCodec}
 tar zxvf ${VideoCodec::-4}/meta-rz-features_codec_*.tar.gz
 
+cd $WORK_DIR/meta-renesas
+patch -p1 < ../extra/0001-Add-HDMI-support-for-RZ-G2.patch
+cd $WORK_DIR
+
 
 if [ -f "${SRC_DIR}/${LinuxBSP}" ]; then
 	### Set up the Yocto Environment and copy a default configuration
-	source poky/oe-init-build-env
-	cp ../meta-renesas/docs/template/conf/rzg2l-dev/*.conf ./conf
+	TEMPLATECONF=$PWD/meta-renesas/meta-rzg2l/docs/template/conf/ source \
+    poky/oe-init-build-env build
+    
+    if [[ -d $1 ]]; then
+        mv $1 build
+    fi
+    # Add Layeres
+    bitbake-layers add-layer ../meta-rz-features/meta-rz-graphics
+    bitbake-layers add-layer ../meta-rz-features/meta-rz-codecs
+    bitbake-layers add-layer ../meta-qt5
 
 	#echo -e "DL_DIR = \"${DL_DIR}\"\n" >> conf/local.conf
 	echo -e "INHERIT += \"rm_work\"\n" >> conf/local.conf
 	echo -e "IMAGE_FSTYPES_remove += \"ext4\"\n" >> conf/local.conf
 
 	# enable GDB Server
-	echo -e "IMAGE_INSTALL_append = \" rpm openssh openssh-sftp-server openssh-scp gdbserver\"\n" conf/local.conf
+	## echo -e "IMAGE_INSTALL_append = \" rpm openssh openssh-sftp-server openssh-scp gdbserver\"\n" conf/local.conf
 
+    if [[ $ENABLE_AI_FRAMEWORK -eq 1 ]]; then
+        echo "ENABLE AI FRAMEWORKS"
+        # Dwonload meta-renesas-ai
+        #git clone https://github.com/mkosinski05/meta-renesas-ai.git ../
+        unzip ${SRC_DIR}/meta-renesas-ai-master.zip -d ${WORK_DIR}
+        mv ${WORK_DIR}/meta-renesas-ai-master ${WORK_DIR}/meta-renesas-ai
+        
+        # Add layer
+        ## bitbake-layers add-layer ${WORK_DIR}/meta-renesas-ai 
+        bitbake-layers add-layer ../meta-renesas-ai
+                
+        sed -i 's/CIP_MODE = "Buster"/CIP_MODE = "None"/g' ./conf/local.conf
+        
+        # Add AI packages to local.conf
+        echo 'IMAGE_INSTALL_append = " armnn-dev armnn-examples armnn-tensorflow-lite-dev armnn-onnx-dev armnn-onnx-examples tensorflow-lite-python"' >> ./conf/local.conf
+        
+        # Add Bencmarks
+        echo 'IMAGE_INSTALL_append = " tensorflow-lite-staticdev tensorflow-lite-dev tensorflow-lite-benchmark armnn-benchmark"' >> ./conf/local.conf
+        echo 'IMAGE_INSTALL_append = " tensorflow-lite-delegate-benchmark"' >> ./conf/local.conf
+
+
+    fi
 	### Build
-	bitbake core-image-bsp
+	MACHINE=smarc-rzg2l  bitbake core-image-weston
 	bitbake core-image-bsp -c populate_sdk
 
 	echo rm -rfd drp ${LinuxBSP::-4} ${VideoCodec::-4} Codec ${Graphic::-4}
