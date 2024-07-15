8db4b86ffc975535416b31d3449b3de9258eef04
zkmike <zkmike@gmail.com>
2024-07-15 15:57:10 -0700

    Updated Yocto Build script for General RZV2L Yocto Builds

--- yocto/rzv2l_build.sh
+++ yocto/rzv2l_build.sh
@@ -9,13 +9,14 @@ VideoCodec=`find ${SRC_DIR} -name RTK0EF0045Z15001ZJ* -printf "%f\n"`
 Graphics=`find ${SRC_DIR} -name RTK0EF0045Z13001ZJ* -printf "%f\n"`
 ISP=`find ${SRC_DIR} -name r11an0561ej* -printf "%f\n"`
 DRP=`find ${SRC_DIR} -name r11an0549ej* -printf "%f\n"`
-CM33=`find ${SRC_DIR} -name r01an6238ej* -printf "%f\n"`
+CM33=`find ${SRC_DIR} -name r01an7254* -printf "%f\n"`
 popd
 
 ENABLE_BUILD=1
-ISP_ENABLED=0
+ISP_ENABLED=1
 EDGEE_ENABLED=0
 ENABLE_AI_FRAMEWORK=0
+ENABLE_INTERNET=0
 
 echo "##############################################################################################"
 
@@ -36,7 +37,7 @@ echo "##########################################################################
 cd $WORK_DIR 
 unzip ${SRC_DIR}/${LinuxBSP}
 pushd ./${LinuxBSP::-4}
-BSP=`find . -name rzv_bsp* -printf "%f\n"`
+BSP=`find . -name rzv_vlp* -printf "%f\n"`
 echo $BSP
 popd
 tar -xf ./${LinuxBSP::-4}/${BSP} -C .
@@ -44,25 +45,28 @@ tar -xf ./${LinuxBSP::-4}/${BSP} -C .
 ### Copy/Move the 'Mali Graphics library' Zip file (RTK0EF0045Z13001ZJ-v0.51_forV2L_EN.zip) under the BSP directory.
 cd $WORK_DIR
 unzip ${SRC_DIR}/${Graphics}
-tar -zxvf ${Graphics::-4}/meta-rz-features_graphics_v1.4.tar.gz
+tar -zxvf ${Graphics::-4}/meta-rz-features_graphics_*.tar.gz
 
 ### Copy/Move the 'MRZG2L Codec Library v0.4' Zip file (RTK0EF0045Z13001ZJ-v0.51_forV2L_EN.zip) under the BSP directory.
 cd $WORK_DIR
 unzip ${SRC_DIR}/${VideoCodec}
-tar zxvf ${VideoCodec::-4}/meta-rz-features_codec_v1.0.1.tar.gz
+tar -zxvf ${VideoCodec::-4}/meta-rz-features_codec_*.tar.gz
 
 ### Setup the RZV2L MultiOS CM33 
 ### Install the and boot commands OpenAMP library
 cd $WORK_DIR
-unzip ${SRC_DIR}/${CM33} -d cm33
-tar zxvf cm33/meta-rz-features.tar.gz
+unzip ${SRC_DIR}/${CM33} 
+pushd ${CM33::-4}
+tar -zxvf meta-rz-features_multi-os_v*.tar.gz -C $WORK_DIR
+popd
+rm -rfd ${CM33::-4}
 
 ### Copy/Move the DRP Support archive file ( rr11an0549ej0500-rzv2l-drpai-sp.zip ) 
 ### Extract the 'DRP-AI Driver Support' package file (rzv2l_meta-drpai_ver0.90.tar.gz) under the BSP directory.
 ### After exacting using the command below, this will add a new directory "meta-drpai" and file "rzv2l-drpai-conf.patch"
 cd $WORK_DIR
 unzip ${SRC_DIR}/${DRP} -d drp
-tar -xvf drp/rzv2l_drpai-driver/meta-rz-features.tar.gz
+tar -xvf drp/rzv2l_drpai-driver/meta-rz-drpai.tar.gz
 
 if [[ $ISP_ENABLED -eq 1 ]]; then
 	### Copy/Move the ISP Support archive file ( r11an0561ej0100-rzv2l-isp-sp.zip ) 
@@ -70,29 +74,33 @@ if [[ $ISP_ENABLED -eq 1 ]]; then
 	### After exacting using the command below, this will add a new directory "meta-isp" and file "rzv2l-isp-conf.patch"
 	cd $WORK_DIR
 	unzip ${SRC_DIR}/${ISP}
-	tar -zxvf ./${ISP::-4}/meta-rz-features.tar.gz
+	tar -zxvf ./r11an0561ej*-rzv2l-isp-sp/meta-rz-*.tar.gz
 fi
 
 ## Reinsert the MultiOS recipe into the meta-rz-features/conf/layers.conf
-cd $WORK_DIR
-sed -i '/demos.inc/a include ${LAYERDIR}/include/openamp/openamp.inc' ./meta-rz-features/conf/layer.conf
+#cd $WORK_DIR
+#sed -i '/demos.inc/a include ${LAYERDIR}/include/openamp/openamp.inc' ./meta-rz-features/conf/layer.conf
 
 ### Set up the Yocto Environment and copy a default configuration
 cd $WORK_DIR
 
-source poky/oe-init-build-env
+TEMPLATECONF=${WORK_DIR}/meta-renesas/meta-rzv2l/docs/template/conf/ source poky/oe-init-build-env build
+bitbake-layers add-layer ../meta-rz-features/meta-rz-graphics
+bitbake-layers add-layer ../meta-rz-features/meta-rz-codecs
+bitbake-layers add-layer ../meta-rz-features/meta-rz-drpai
+bitbake-layers add-layer ../meta-rz-features/meta-rz-simple-isp
+bitbake-layers add-layer ../meta-rz-features/meta-rz-multi-os
 
-cp ../meta-renesas/docs/template/conf/smarc-rzv2l/*.conf ./conf/
-
-
-echo -e "DL_DIR = \"${DL_DIR}\"\n" >> conf/local.conf
+if [[ $ENABLE_INTERNET -eq 0 ]];then 
+ echo -e "DL_DIR = \"${DL_DIR}\"\n" >> conf/local.conf
+fi
 echo -e "INHERIT += \"rm_work\"\n" >> conf/local.conf
 echo -e "IMAGE_FSTYPES_remove += \"ext4\"\n" >> conf/local.conf
 
 ## Add Support for Application Debugging
 # echo -e "IMAGE_INSTALL_append = \" rpm openssh openssh-sftp-server openssh-scp gdbserver\""  >> conf/local.conf
 ##  Required to Support LVGL GUI Framework
-echo -e "IMAGE_INSTALL_append = \" libsdl2-dev\""  >> conf/local.conf
+#echo -e "IMAGE_INSTALL_append = \" libsdl2-dev\""  >> conf/local.conf
 
 if [[ $EDGEE_ENABLED -eq 1 ]]; then
     echo "ENABLE EDGE IMPULSE"
@@ -115,11 +123,11 @@ fi
 
 if [[ $ENABLE_BUILD -eq 1 ]]; then
 ### Build
-bitbake core-image-weston
+MACHINE=smarc-rzv2l bitbake core-image-weston
 
 echo "TOOLCHAIN_TARGET_TASK += \"kernel-devsrc\"" >> ./conf/local.conf
-#
-bitbake core-image-weston -c populate_sdk
+
+MACHINE=smarc-rzv2l bitbake core-image-weston -c populate_sdk
 fi
 
 echo rm -rfd drp ${LinuxBSP::-4} ${VideoCodec::-4} ${Graphics::-4} ${ISP::-4} cm33 
